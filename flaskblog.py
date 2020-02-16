0f56dce6058fc0dc081997a9127db2bfb48fb8c8
chingun <chingun@umich.edu>
2020-02-16 00:07:34 -0800

    Better website

--- flaskblog.py
+++ flaskblog.py
@@ -1,55 +1,68 @@
 from flask import Flask, render_template, url_for
 app = Flask(__name__)       #special variable that is just the name of the module __name__ is same as __main__
 
-
-embedded_posts = [
-	{
-		'class': 'Embedded Systems',
-		'subclass': 'Software',
-		'link': 'https://www.eecs.umich.edu/courses/eecs473/labs.html',
-		'link_about': 'Advanced Embedded Systems',
-		'more_on_topic': ': University of Michigan. ',
-		'date': 'December 2017',
-	},
-	{
-		'class': 'Embedded Systems',
-		'subclass': 'Tools',
-		'link': 'http://www.kicad-pcb.org/',
-		'link_about': 'KiCad PCB Design',
-		'more_on_topic': ': free tool for designing PCBs',
-		'date': 'December 2017',
-	},
-	{
-		'class': 'Embedded Systems',
-		'subclass': 'Libraries',
-		'link': 'https://www.freertos.org/',
-		'link_about': 'freeRTOS',
-		'more_on_topic': ': free real time operating system protected under open source license',
-		'date': 'December 2017',
-	}
+class custom_post:
+    title = ''
+    image_link = ''
+    num_posts = 0
+    each_posts = []
+    
+    def __init__(self, title, image_link, num_posts, posts):
+        self.title = title
+        self.image_link = image_link
+        self.num_posts = num_posts
+        self.each_posts = posts
+        return
+    
+    def __append_post__(post):
+        self.each_posts.append(post)
+        self.num_posts = self.num_posts + 1
+        return
+        
+embedded_initial_posts = [
+    {
+        'subclass': 'Software',
+        'link': 'https://www.eecs.umich.edu/courses/eecs473/labs.html',
+        'link_about': 'Advanced Embedded Systems',
+        'more_on_topic': ': University of Michigan. ',
+        'date': 'December 2017',
+    },
+    {
+        'subclass': 'Tools',
+        'link': 'http://www.kicad-pcb.org/',
+        'link_about': 'KiCad PCB Design',
+        'more_on_topic': ': free tool for designing PCBs',
+        'date': 'December 2017',
+    },
+    {
+        'subclass': 'Libraries',
+        'link': 'https://www.freertos.org/',
+        'link_about': 'freeRTOS',
+        'more_on_topic': ': free real time operating system protected under open source license',
+        'date': 'December 2017',
+    }
 ]
-
-ai_posts = [
+embedded_posts = custom_post('Embedded Systems', '', 3, embedded_initial_posts)
+ai_inital_posts = [
 	{
-		'class': 'AI/ML',
 		'subclass': 'Reinforcement Learning',
-		'link': 'https://www.freertos.org/',
-		'link_about': 'freeRTOS',
-		'more_on_topic': ': free real time operating system protected under open source license',
+		'link': '',
+		'link_about': '',
+		'more_on_topic': 'Nothing to write here yet. Here will lay my AI/ML Projects',
 		'date': 'December 2017',
 	}
 ]
-
+ai_posts = custom_post('AI/ML', 'static/aigif.gif', 1, ai_inital_posts)
 app_posts = [
-	{
-		'class': 'AI/ML',
-		'subclass': 'Reinforcement Learning',
-		'link': 'https://www.freertos.org/',
-		'link_about': 'freeRTOS',
-		'more_on_topic': ': free real time operating system protected under open source license',
-		'date': 'December 2017',
+	{ 
+		'subclass': 'iOS Applications',
+		'link': '',
+		'link_about': '',
+		'more_on_topic': 'Here lays all my applications which is 0 right now',
+                'date': 'December 2017',
 	}
 ]
+app_posts = custom_post('Mobile Applications', 'static/codegif.gif', 1, app_posts)
 
 @app.route('/')             # handle all background for home page of website
 @app.route('/home')   		# 
@@ -59,15 +72,15 @@ def hello():
 
 @app.route('/embedded')     # Behind on the link http://127.0.0.1:5000/projects
 def embedded():
-	return render_template('projects/embedded/index.html', title="Embedded Systems", posts=embedded_posts)
+	return render_template('projects/embedded/index.html', posts=embedded_posts)
 
 @app.route('/apps')
 def apps():
-	return render_template('projects/apps/index.html', title='Applications', posts=app_posts)
+	return render_template('projects/apps/index.html', posts=app_posts)
 
 @app.route('/ai')
 def ai():
-	return render_template('projects/ai/index.html', title='Artificial Intelligence', posts=ai_posts)
+	return render_template('projects/ai/index.html', posts=ai_posts)
 
 
 if __name__ == '__main__': 	# True only if this module is run directly 
